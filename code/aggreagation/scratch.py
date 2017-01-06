60542a6418dfce206cea97e7d83b670e1dfb85f4
Joseph Blodgett <Joseph.blodgett@gmail.com>
2017-01-06 15:48:25 -0700

    Aggregation saving to csv

--- code/aggreagation/scratch.py
+++ code/aggreagation/scratch.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 from datetime import timedelta
 
+#test = sc.textFile("/home/sir/Neighborhoods/Data/Test/test_bucket_times.csv")
 test = sc.textFile("/home/sir/Neighborhoods/Data/Taxi/2016-out/1-yellow-out.csv")
+outputDir = "/home/sir/Neighborhoods/Data/Test/Buckets-out/"
+
 
 Header = test.filter(lambda l: "tpep_pickup" in l)
 pruned = test.subtract(Header)
@@ -25,6 +28,7 @@ def BucketTime(x):
 array2 = array1.map(lambda x: BucketTime(x))
 #map to county + Tract + block + day of week + hour + minute (Bucket))
 array3 = array2.map(lambda x: (x[6]+":"+x[7]+":"+x[8]+"-"+str(x[12])+":"+str(x[0].hour)+":"+str(x[0].minute), 1))
+#count 
 step4 = array3.reduceByKey( lambda a,b: a+b)
 
 #Map these now to a list of county/tract/block with week hour minute county
@@ -33,23 +37,23 @@ def SplitOnCounty(x):
     toReturn = []
     splitVals = x[0].split("-")
     toReturn.append(splitVals[0])
-    date = datetime.strptime(splitVals[1], "%w:%H:%M")    
-    toReturn.append(([date],[x[1]]))
+    toReturn.append(([splitVals[1]],[x[1]]))
     return toReturn
 
+#build a block specific count 
 step5 = step4.map( lambda x: SplitOnCounty(x))    
 step6 = step5.reduceByKey( lambda a,b: (a[0] + b[0], a[1]+b[1]))
 
 data = step6.collect()
 
-
-
-import matplotlib
-matplotlib.use('Agg')
-from matplotlib import pyplot
-
-def mapData(Data):
-    dates = matplotlib.dates.date2num(Data[1][0])
-    pyplot.plot_date(dates, Data[1][1])
-    pyplot.savefig("/home/sir/Neighborhoods/Data/"+Data[0].replace(":", "-")+".png")
-
+import csv 
+for curValue in data:
+    fileName = curValue[0].replace(":", "-")
+    with open(outputDir + fileName+".csv", "w+") as f:
+        writer = csv.writer(f)
+        writer.writerow(["weekday", "time", "trip_count"])
+        for i in range(len(curValue[1][0])):
+            dayTime = curValue[1][0][i]
+            value = curValue[1][1][i]
+            loc = dayTime.index(":")
+            writer.writerow([dayTime[:loc], dayTime[loc+1:],value])
